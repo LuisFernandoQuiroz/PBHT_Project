37a9a671d01e15a6101cf3c75ead574e9c353a38
LuisFernandoQuiroz <126516213+LuisFernandoQuiroz@users.noreply.github.com>
2024-04-16 16:46:59 -0500

    Added initial build (unfinished)
    Added the first files to the build, currently unfinished, but working with setup.

---
+++ src/main/java/Model/model_1.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class model_1 {
+	private String user;
+	private String d;
+	
+	public model_1(String u) {
+		this.setUser(u);
+	}
+	
+	public void setUser(String u) {
+		this.user = u;
+	}
+}
